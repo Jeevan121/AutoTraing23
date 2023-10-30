43822242bf71d8938df966e62ee8930a8ac5ec4e
Jeev Devops <jkborale@gmail.com>
2023-10-30 11:28:23 -0700

    demo script

---
+++ src/com/tests/FirstDay.java
@@ -0,0 +1,35 @@
+package com.tests;
+
+public class FirstDay {
+	
+	public static void main(String[] agrs) {
+		// a +b
+		
+		 int a = (int) 11.15;
+		 int b= (int) 4.11;
+		 
+		 int sum=a+b;
+		 System.out.println("the result is::"+sum);
+		 
+		 //String
+		 String test ="Hello welcome to java";
+		 System.out.println(test);
+		 char ch = 'A';
+		 System.out.println(ch);
+		 
+		 String world = " World";
+		 
+		 String resut=test+world;
+		 System.out.println(resut);
+		 
+		 boolean tes = true;
+		 System.out.println(tes);
+		 
+		 boolean tes1 = false;
+		 System.out.println(tes1);
+		 
+		 
+		
+	}
+
+}
